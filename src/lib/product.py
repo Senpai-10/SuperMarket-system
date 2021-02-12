3d3e124bff71ead84a7aa728a785065b9a3e2285
Senpai-10 <bmjfdrh@gmail.com>
2021-02-12 18:26:54 +0300

    UPDATE method

--- src/lib/product.py
+++ src/lib/product.py
@@ -51,7 +51,48 @@ class product:
         self.conn.commit()
 
     def UPDATE(self):
-        ...
+        print("""
+1. UPDATE NAME      : update product name 
+2. UPDATE PRICE     : update product price
+3. UPDATE QUANTITY  : update product quantity
+
+! enter option number.
+        """)
+
+        command = input("$ ")
+        print("")
+
+        if command in ['1', '2', '3']:
+
+            print("enter product ( id )")
+            productID = input("$ ")
+            print("")
+
+            print("\nOLD product data\n")
+
+            self.cursor.execute(f"select * from products where id = :id", {'id': productID})
+            print("id\tname\t\tprice\t\tquantity\n")
+            row = self.cursor.fetchone()
+            print(f'{row[0]}\t{row[1]}\t\t{row[2]}\t\t{row[3]}\n')
+
+            if command == '1':
+                NEW_NAME = input("NEW NAME $ ")
+                self.cursor.execute("UPDATE products SET name = :new  WHERE id = :id", {'new': NEW_NAME, 'id': productID})
+                self.conn.commit()
+
+            if command == '2':
+                NEW_PRICE = input("NEW PRICE $ ")
+
+                self.cursor.execute("UPDATE products SET price = :new WHERE id = :id", {'new': NEW_PRICE, 'id': productID})
+                self.conn.commit()
+
+            if command == '3':
+                NEW_QUANTITY = input("NEW QUANTITY $ ")
+
+                self.cursor.execute("UPDATE products SET quantity = :new WHERE id = :id", {'new': NEW_QUANTITY, 'id': productID})
+                self.conn.commit()
+
+        else: print("wrong entry")
 
     def REMOVE(self):
         ...
